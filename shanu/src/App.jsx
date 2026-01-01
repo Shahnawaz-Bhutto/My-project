3b57e2a8f8192dc3e93e35561204a54b4784409f
Shahnawaz-Bhutto <hajibooshanu777@gmail.com>
2026-01-01 14:43:47 +0500

    My first react project

--- shanu/src/App.jsx
+++ shanu/src/App.jsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {/* =====Header 1 Start===== */}
+      <div className="firstheader">
+        <div className="ecourses">
+          <h1>ECOURCES</h1>
+        </div>
+        <div className="office">
+          <h3>Office</h3>
+          <p>Next planer Acadmay (skd), Gilgit Baltistan, Pakistan</p>
+        </div>
+        <div className="gmail">
+          <h3>Gmail</h3>
+          <p>shahnawazbhutto000@gmail.com</p>
+        </div>
+        <div className="contact">
+          <h3>Contact</h3>
+          <p>+923555869818</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <hr />
+      {/* =====Header 1 End===== */}
+      {/* =====Header 2 Start===== */}
+      <div className="secondheader">
+      <div className="subject">
+        <h1>Subject</h1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="tags">
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Courses</a>
+        <a href="">Developers</a>
+        <a href="">Blogs</a>
+        <a href="">Contact</a>
+        <button><p>joint us</p></button>
+      </div>
+      </div>
+      <div className="photo"></div>
     </>
   )
 }
