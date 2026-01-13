e9a75eb2bf78b6147d74a3235786ed1edfc18279
Shahnawaz-Bhutto <hajibooshanu777@gmail.com>
2026-01-13 15:44:33 +0500

    first commit

--- shanu/src/App.jsx
+++ shanu/src/App.jsx
@@ -5,14 +5,29 @@ import Header from '../src/Components/Header'
 function App() {
   return (
     <div>
-      <Header />
-      <div className="photo">
+      {/* <Header /> */}
+      {/* <div className="photo">
         <h2>Best Online Course</h2><br /><br />
         <h1>Best Eduacation From </h1><br /><h3>Your Home</h3>
         <br />
         <button><p>Learn more</p></button>
-      </div>
+      </div> */}
+
+
+      <BottomNavigation
+  showLabels
+  value={value}
+  onChange={(event, newValue) => {
+    setValue(newValue);
+  }}
+>
+  <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+  <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+  <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+</BottomNavigation>
     </div>
+
+
   )
 }
 
